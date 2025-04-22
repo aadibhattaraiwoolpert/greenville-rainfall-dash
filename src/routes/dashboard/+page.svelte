1393d12c5051650b2b6502b98e4a8bc9225b0407
Aaditya <aadi.bhattarai@woolpert.com>
2025-04-22 17:14:29 -0400

    api env check

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,7 +3,7 @@
 	import Legend from './legend.svelte';
 	import CardGroup from './card-group.svelte';
 
-	const API_KEY = 'AIzaSyDMygtGKBDEbdbt0Q2Xnwhg9pxPH-mJ-Ho';
+	const API_KEY = import.meta.env.VITE_API_KEY;
 	const SHEET_ID = '1r4C0aDzq21_Nl9szpqd_TweC2785EY98Jt8GaOC0am4';
 	const SHEET_RANGE = 'Rainfall Data!A1:E100';
 
