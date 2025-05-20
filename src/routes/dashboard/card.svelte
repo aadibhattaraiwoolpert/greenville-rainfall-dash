7008e16ef15ce9a4186c118538e7811859cc7842
Aaditya <aadi.bhattarai@woolpert.com>
2025-05-20 00:35:14 -0400

    maybe the card change color will work

--- src/routes/dashboard/card.svelte
+++ src/routes/dashboard/card.svelte
@@ -1,70 +1,66 @@
 <script>
-	import RainIcon from './rain-icon.svelte';
+    import RainIcon from './rain-icon.svelte';
 
-	export let amount = 0.0;
-	export let unit = 'in.';
-	export let title = 'Rainfall 1-hr';
+    export let amount = 0.0;
+    export let unit = 'in.';
+    export let title = 'Rainfall 1-hr';
+    export let color = '#00649a';
 </script>
 
-<div class="card">
-	<div class="title">
-		<span class="title-text"> {title}</span>
-	</div>
+<div class="card" style="background-color: #{color};">
+    <div class="title">
+        <span class="title-text"> {title}</span>
+    </div>
 
-	<div class="content">
-		<div class="rain-amount">
-			<div><RainIcon size="30"></RainIcon></div>
-			<div class="amount-text">{amount} {unit}</div>
-		</div>
-	</div>
+    <div class="content">
+        <div class="rain-amount">
+            <div><RainIcon size="30"></RainIcon></div>
+            <div class="amount-text">{amount} {unit}</div>
+        </div>
+    </div>
 </div>
 
 <style>
-	.card {
-		display: flex;
-		flex-direction: column;
+    .card {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        margin: 0 0.5rem;
+        padding: 1rem;
+        border-radius: 4px;
+        /* Removed background-color from here */
+    }
 
-		flex: 1;
-		margin: 0 0.5rem;
-		padding: 1rem;
+    .title {
+        border-bottom-color: #ccc;
+        border-bottom-width: 1px;
+        border-bottom-style: solid;
+        padding: 0.3rem 0;
+        margin-bottom: 1rem;
+    }
 
-		background: #00649a;
-		border-radius: 4px;
-	}
+    .title-text {
+        font-size: 1rem;
+        font-weight: 400;
+    }
 
-	.title {
-		border-bottom-color: #ccc;
-		border-bottom-width: 1px;
-		border-bottom-style: solid;
-		padding: 0.3rem 0;
-		margin-bottom: 1rem;
-	}
+    .content {
+        padding: 0.25rem 0rem;
+    }
 
-	.title-text {
-		font-size: 1rem;
-		font-weight: 400;
-	}
+    .rain-amount {
+        flex: 1;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin: 0 0.5rem;
+        align-items: center;
+        width: 100%;
+        gap: 0.5rem;
+    }
 
-	.content {
-		padding: 0.25rem 0rem;
-	}
-
-	.rain-amount {
-		flex: 1;
-
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-
-		margin: 0 0.5rem;
-		align-items: center;
-		width: 100%;
-
-		gap: 0.5rem;
-	}
-
-	.amount-text {
-		font-size: 2rem;
-		padding-right: 0.5rem;
-	}
+    .amount-text {
+        font-size: 2rem;
+        padding-right: 0.5rem;
+    }
 </style>
