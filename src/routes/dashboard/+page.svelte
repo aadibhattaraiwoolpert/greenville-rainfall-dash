b0e0570af9d4f612b5230432b2db3396163b5712
Aaditya <aadi.bhattarai@woolpert.com>
2025-08-06 11:07:56 -0400

    new google ac new link

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,7 +4,7 @@
 	import CardGroup from './card-group.svelte';
 
 	const API_KEY = import.meta.env.VITE_API_KEY;
-	const SHEET_ID = '1KrryzzQF33XR2FRKOboJHnlJvTH27qGB8vIAEFOIOi8'; 
+	const SHEET_ID = '1KrryzzQF33XR2FRKOboJHnlJvTH27qGB8vIAEFOIOi8'; //new spreadsheet
 	const SHEET_RANGE = 'Rainfall Data!A1:P100'; //For 5 cards
 
 	let data = [];
